9fe9c9d12024dcf792c2817f23e0dc6833e40894
tanloc109 <phamtanloc1009@gmail.com>
2025-01-08 13:16:32 +0700

    init project

---
+++ src/main/java/com/foodygo/FoodygoApplication.java
@@ -0,0 +1,13 @@
+package com.foodygo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodygoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodygoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foodygo/FoodygoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodygo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodygoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
