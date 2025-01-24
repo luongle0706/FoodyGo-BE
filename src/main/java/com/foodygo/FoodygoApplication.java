4d7f5bd8c6294b4a6c761c7bc06184e1f3f12579
AnhNT <anhntse183225@fpt.edu.vn>
2025-01-24 23:54:15 +0700

    Test timezone

--- src/main/java/com/foodygo/FoodygoApplication.java
+++ src/main/java/com/foodygo/FoodygoApplication.java
@@ -3,10 +3,13 @@ package com.foodygo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FoodygoApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+07:00"));
         SpringApplication.run(FoodygoApplication.class, args);
     }
 
