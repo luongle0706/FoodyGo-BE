4b2bb569471f4aa6a1d10e5c7399fdbf3cbcbd80
AnhNT <theanh08042004@gmail.com>
2025-03-03 12:43:22 +0700

    Testing file upload

--- src/main/java/com/foodygo/configuration/WebSecurityConfig.java
+++ src/main/java/com/foodygo/configuration/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,7 +19,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(securedEnabled = true)
 @Slf4j
 public class WebSecurityConfig {
 
---
+++ src/main/java/com/foodygo/controller/test/ImageController.java
@@ -0,0 +1,44 @@
+package com.foodygo.controller.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@RestController
+@RequestMapping("/api/v1/images")
+@RequiredArgsConstructor
+public class ImageController {
+
+    @Value("${image.saveLocation}")
+    private String uploadDir;
+
+    @Value("${image.getLocation}")
+    private String getLocation;
+
+    @PostMapping
+    public ResponseEntity<String> uploadImage(
+            @RequestParam("file") MultipartFile file
+    ) {
+        try {
+            String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+            Path filePath = Paths.get(uploadDir, fileName);
+            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+            String fileUrl = getLocation + fileName;
+            return ResponseEntity.ok(fileUrl);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
