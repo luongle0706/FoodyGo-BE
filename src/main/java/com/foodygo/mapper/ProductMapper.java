857e2a1bdfac4a3231348ba5854694f60f2812e3
quannmin <laogiado2003@gmail.com>
2025-03-13 11:37:34 +0700

    update product

--- src/main/java/com/foodygo/mapper/ProductMapper.java
+++ src/main/java/com/foodygo/mapper/ProductMapper.java
@@ -4,6 +4,7 @@ import com.foodygo.dto.ProductDTO;
 import com.foodygo.entity.Product;
 import com.foodygo.entity.Restaurant;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
@@ -12,5 +13,7 @@ import org.mapstruct.factory.Mappers;
 public interface ProductMapper {
 
     ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+    @Mapping(source = "image", target = "image")
+    @Mapping(source = "restaurant.id", target = "restaurantId")
     ProductDTO toDTO(Product product);
 }
